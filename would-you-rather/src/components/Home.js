7cbb4469c63caf41b6553081e1951482f2b6321f
Diana <a0132066@gmail.com>
2020-07-26 13:58:20 -0400

    feat: Redirect if no user is logged in

--- would-you-rather/src/components/Home.js
+++ would-you-rather/src/components/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 const Home = () => {
+
+  const authedUser = useSelector(state => {
+    return state.authedUser
+  });
+
   return (
-    <div>HOME</div>
+    authedUser ?
+    <div>HOME</div> :
+    <Redirect to="/"/>
   )
 }
 
--- would-you-rather/src/components/Nav.js
+++ would-you-rather/src/components/Nav.js
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import { NavLink } from 'react-router-dom'
-import { Layout, Menu, Avatar } from 'antd'
-import { UserOutlined } from '@ant-design/icons'
+import { useSelector } from 'react-redux'
+import { Layout, Menu, Avatar, Result, Button } from 'antd'
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons'
 
 const Nav = () => {
 
   const { Header } = Layout
 
+  const userName = useSelector(state => {
+    if(state.authedUser){
+      return state.users[state.authedUser].name
+    }else{
+      return false
+    }
+  });
+
   return (
     <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-      <div className="logo">
-          <Avatar icon={<UserOutlined />} />
-          <span className="user-name">Diana</span>
-      </div>
-      <Menu theme="dark" mode="horizontal">
-        <Menu.Item key="1">
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key="2">New Question</Menu.Item>
-        <Menu.Item key="3">Leader Board</Menu.Item>
-      </Menu>
+      {
+          userName &&
+          <Fragment>
+            <div className="logo">
+                <Avatar icon={<UserOutlined />} />
+                <span className="user-name">{userName}</span>
+                <Button type="text" style={{ color: '#fff' }}>
+                  <LogoutOutlined />
+                </Button>
+            </div>
+
+            <Menu theme="dark" mode="horizontal">
+              <Menu.Item key="1">
+                <NavLink to='/home' exact>
+                  Home
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="2">New Question</Menu.Item>
+              <Menu.Item key="3">Leader Board</Menu.Item>
+            </Menu>
+          </Fragment>
+      }
     </Header>
   )
 }
